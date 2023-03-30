7f699a164e447e9c66dde6aafe884e957c75ef8e
Silas Junior <silas.mcoach@gmail.com>
2023-03-29 23:48:07 -0300

    feat: create routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
+import "reflect-metadata"
+import "express-async-errors"
 import express from 'express';
+import contactRoutes from './routes/contact.routes';
+import sessionRoutes from './routes/session.routes';
+import userRoutes from './routes/user.routes';
 
 
 const app = express();
 app.use(express.json());
 
+app.use("/users", userRoutes)
+app.use("/session", sessionRoutes)
+app.use("/contacts", contactRoutes)
 
 export default app;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,1 +1,11 @@
-//init
+import { Router } from "express";
+
+const userRoutes = Router();
+
+userRoutes.post("")
+userRoutes.get("")
+userRoutes.patch("/:id")
+userRoutes.delete("/:id")
+userRoutes.patch("/:id/image")
+
+export default userRoutes;
