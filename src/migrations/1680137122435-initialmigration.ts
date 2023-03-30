96b81d87b8c4c6c84a2e233b336ae38259cb7373
Silas Junior <silas.mcoach@gmail.com>
2023-03-29 22:31:02 -0300

    start code entities

---
+++ src/migrations/1680137122435-initialmigration.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class initialmigration1680137122435 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
